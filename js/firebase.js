69844679743fb385fc72a530cc9630da53ced961
VituPeralta <vitor.mmicheli@gmail.com>
2021-12-14 20:28:41 -0300

    Limpar firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCtjoqZhejTcWmiuYs4ROSJSsTy-wqLvYY",
-    authDomain: "plataformapizza.firebaseapp.com",
-    projectId: "plataformapizza",
-    storageBucket: "plataformapizza.appspot.com",
-    messagingSenderId: "363663914755",
-    appId: "1:363663914755:web:150c86aa0c762f1c6e5b36",
-    measurementId: "G-1NC277TS3F"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   //inicializar o firebase
